c7f73d01e0c2b5c1a6ea0403c015b79814cb5f32
Marc <mcalafellsmx@gmail.com>
2016-12-13 17:59:19 +0100

    mocking repository

--- tests/StudiesControllerTest.php
+++ tests/StudiesControllerTest.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Scool\Curriculum\Repositories\StudyRepository;
 
 class StudiesControllerTest extends TestCase
 {
@@ -14,6 +15,17 @@ class StudiesControllerTest extends TestCase
 
     use DatabaseMigrations;
 
+    protected $repository;
+
+    public function setUp() {
+     //   $this->login();
+        $this->repository = Mockery::mock(StudyRepository::class);
+    }
+
+    public function tearDown() {
+        Mockery::close();
+    }
+
     public function testExample()
     {
         $this->assertTrue(true);
@@ -24,16 +36,36 @@ class StudiesControllerTest extends TestCase
 
     }
 
+    /**
+     *
+     */
     public function testIndex() {
-        $user=factory(App\User::class)->create();
-        $studies=factory(Scool\Curriculum\Models\Study::class,50)->create();
+        $this->login();
         $this->actingAs($user);
         $this->get('studies');
         $this->assertResponseOk();
 
         $this->assertViewHas('studies');
 
-        $studies= $this->response->getOriginalContent()->getData();
+        $studies= $this->response->getOriginalContent()->getData()['studies'];
+
+        $this->assertInstanceOf(Illuminate\Database\Eloquent\Collection::class, $studies);
+    }
+
+public function testStore(){
+    list($user, $studies) = $this->login();
+    $this->post('studies');
+    $this->assertRedirectedToRoute();
+}
+
+    /**
+     * @return array
+     */
+    public function login()
+    {
+        $user = factory(App\User::class)->create();
+        $studies = factory(Scool\Curriculum\Models\Study::class, 50)->create();
+        return array($user, $studies);
     }
 
 
