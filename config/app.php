1490c8e1afbd9bc636a3d14fa66d8ae877f8271e
Marc <mcalafellsmx@gmail.com>
2016-12-20 19:09:24 +0100

    TMTFOC

--- config/app.php
+++ config/app.php
@@ -195,6 +195,7 @@ return [
         Barryvdh\Debugbar\ServiceProvider::class,
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Spatie\Menu\Laravel\MenuServiceProvider::class,
         #llum_providers
 
     ],
@@ -253,7 +254,9 @@ return [
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Menu' => Spatie\Menu\Laravel\MenuFacade::class,
+        'Link' => Spatie\Menu\Laravel\Link::class,
+        'Html' => Spatie\Menu\Laravel\Html::class,
         #llum_aliases
 
     ],
--- resources/views/studies/index.blade.php
+++ resources/views/studies/index.blade.php
@@ -10,68 +10,92 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
 
-                <div class="box box-default collapsed-box">
+                <div class="box box-default">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Add studia</h3>
+                        <h3 class="box-title">Create Study</h3>
 
                         <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
                             </button>
                         </div>
+                        <!-- /.box-tools -->
                     </div>
-                    <div class="box-body" style="display: none;">
-
+                    <!-- /.box-header -->
+                    <div class="box-body">
                         <div class="alert alert-success alert-dismissible">
                             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            <h4><i class="icon fa fa-check"></i> Alert!</h4>
-                            Studi created de puta mare.
+                            <h4><i class="icon fa fa-check"></i> Done!</h4>
+                            Study create ok!
+                        </div>
+                        <div class="alert alert-danger alert-dismissible">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <h4><i class="icon fa fa-check"></i> Oops!</h4>
+                            Something is wrong!
                         </div>
 
                         <form role="form" action="/studies" method="post">
-                            {{csrf_field()}}
-                            <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Enter the name of studie">
-                            </div>
+                            <!-- text input -->
+                            {{ csrf_field() }}
 
+                            <input type="hidden" name="law_id" value="1">
+
+                            @if ($errors->has('name'))
+                                <div class="form-group has-warning">
+                                    <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i>Name</label>
+                                    <input type="text" class="form-control" id="inputWarning" placeholder="Name" name="name">
+                                    @foreach ($errors->get('name') as $message)
+                                        <span class="help-block"> {{ $message }}</span>
+                                    @endforeach
+                                </div>
+                            @else
+                                <div class="form-group">
+                                    <label>Name</label>
+                                    <input type="text" class="form-control" placeholder="Name" name="name">
+                                </div>
+                            @endif
+
+
+
+
+                            <input type="submit" value="create">
 
                             <!-- input states -->
-                            <div class="form-group has-success">
-                                <label class="control-label" for="inputSuccess"><i class="fa fa-check"></i>Name</label>
-                                <input type="text" class="form-control" id="inputSuccess" placeholder="Enter ...">
-                                <span class="help-block">success</span>
-                            </div>
-                            <div class="form-group has-warning">
-                                <label class="control-label" for="inputWarning"><i class="fa fa-bell-o"></i> Name</label>
-                                <input type="text" class="form-control" id="inputWarning" placeholder="Enter ...">
-                                <span class="help-block">warning</span>
-                            </div>
-                            <div class="form-group has-error">
-                                <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Name</label>
-                                <input type="text" class="form-control" id="inputError" placeholder="Enter ...">
-                                <span class="help-block">error</span>
-                            </div>
+                            {{--<div class="form-group has-success">--}}
+                            {{--<label class="control-label" for="inputSuccess"><i class="fa fa-check"></i> Input with success</label>--}}
+                            {{--<input type="text" class="form-control" id="inputSuccess" placeholder="Enter ...">--}}
+                            {{--<span class="help-block">Help block with success</span>--}}
+                            {{--</div>--}}
 
-                            <input type="submit">
-
-
+                            {{--<div class="form-group has-error">--}}
+                            {{--<label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i> Input with--}}
+                            {{--error</label>--}}
+                            {{--<input type="text" class="form-control" id="inputError" placeholder="Enter ...">--}}
+                            {{--<span class="help-block">Help block with error</span>--}}
+                            {{--</div>--}}
 
                         </form>
-                    </div>
 
+                    </div>
+                    <!-- /.box-body -->
                     <div class="box-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button class="btn btn-primary" type="submit">Create</button>
                     </div>
-
-
                 </div>
 
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Studies</h3>
 
-
-                <div class="panel panel-default">
-                    <div class="panel-heading">Studies</div>
-                    <div class="panel-body">
-
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+                            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="box-body">
                         <table class="table table-bordered">
                             <thead>
                             <tr>
@@ -90,8 +114,8 @@
                             </tbody>
 
                         </table>
-
                     </div>
+
                 </div>
             </div>
         </div>
